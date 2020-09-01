34fa65b4d036735101cd522cbbdfe61b959d5814
untletch <mwamathai99@gmail.com>
2020-09-01 15:35:48 +0300

    day 4 solution

---
+++ day4/day4.py
@@ -0,0 +1,49 @@
+"""
+right for part 1
+wrong for part 2
+"""
+
+
+def comp(n):
+    s = str(n)
+    for i in range(len(s) - 1):
+        if int(s[i]) > int(s[i + 1]):
+            return False
+    return True
+
+
+def rep(n):
+    s = str(n)
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            return True
+    return False
+
+
+def num_rep(n):
+    count = 1
+    s = str(n)
+    ans = {}
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            count += 1
+            ans[s[i]] = count
+        else:
+            count = 1
+    for i in ans.values():
+        if i % 2 == 1 and len(ans) == 1:
+            return False
+    return True
+
+
+def sol():
+    count = 0
+    for i in range(152085, 670283):
+        if len(set(str(i))) == len(str(i)):
+            continue
+        if comp(i) and rep(i) and num_rep(i):
+            count += 1
+    return count
+
+
+print(sol())
